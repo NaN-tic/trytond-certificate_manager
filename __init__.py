fb5a5fd9fefa8dd17c6db58c22f6112b83dedf7e
Raimon Esteve <raimonesteve@gmail.com>
2023-03-10 12:21:44 +0100

    Load PKCS12

--- __init__.py
+++ __init__.py
@@ -4,12 +4,17 @@
 from trytond.pool import Pool
 from . import certificate_manager
 from . import company
+from . import load_pkcs12
 
 def register():
     Pool.register(
         certificate_manager.CertificateManager,
+        load_pkcs12.LoadPKCS12Start,
         module='certificate_manager', type_='model')
     Pool.register(
         company.CertificateManager,
         module='certificate_manager', type_='model',
         depends=['company'])
+    Pool.register(
+        load_pkcs12.LoadPKCS12,
+        module='certificate_manager', type_='wizard')
---
+++ load_pkcs12.py
@@ -0,0 +1,83 @@
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
+from io import BytesIO
+from logging import getLogger
+
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.serialization import pkcs12
+from cryptography.exceptions import UnsupportedAlgorithm
+
+from trytond.transaction import Transaction
+from trytond.pool import Pool
+from trytond.model import ModelView
+from trytond.model import fields
+from trytond.wizard import Wizard
+from trytond.wizard import StateView
+from trytond.wizard import StateTransition
+from trytond.wizard import Button
+from trytond.i18n import gettext
+from trytond.exceptions import UserError
+
+__all__ = [
+    'LoadPKCS12',
+    'LoadPKCS12Start',
+]
+_logger = getLogger(__name__)
+
+
+class LoadPKCS12Start(ModelView):
+    "Load PKCS12 Start"
+    __name__ = "certificate.manager.load_pkcs12.start"
+
+    pfx = fields.Binary('PFX File', required=True)
+    password = fields.Char('Password', required=True)
+
+
+class LoadPKCS12(Wizard):
+    "Load PKCS12"
+    __name__ = "certificate.manager.load_pkcs12"
+    start = StateView(
+        'certificate.manager.load_pkcs12.start',
+        'certificate_manager.certificate_manager_load_pkcs12_start_view', [
+            Button('Cancel', 'end', 'tryton-cancel'),
+            Button('Load', 'load', 'tryton-ok', default=True),
+        ])
+    load = StateTransition()
+
+    def transition_load(self):
+        CertificateManager = Pool().get('certificate.manager')
+
+        with BytesIO(self.start.pfx) as pfx:
+            try:
+                (
+                    private_key,
+                    certificate,
+                    additional_certificates,
+                ) = pkcs12.load_key_and_certificates(
+                    pfx.read(), self.start.password.encode()
+                )
+                key = private_key.private_bytes(
+                    encoding=serialization.Encoding.PEM,
+                    format=serialization.PrivateFormat.PKCS8,
+                    encryption_algorithm=serialization.NoEncryption(),
+                )
+                crt = certificate.public_bytes(serialization.Encoding.PEM)
+                CertificateManager.write(self.records, {
+                    'pem_certificate': crt,
+                    'private_key': key,
+                })
+                _logger.info(
+                    'Correctly loaded SSL credentials for company %s',
+                    company.rec_name)
+            except (ValueError, TypeError, UnsupportedAlgorithm) as e:
+                _logger.debug('Cryptographic error loading pkcs12: %s', e)
+                errors = e.args[0]
+                if isinstance(errors, list):
+                    message = ', '.join(error[2] for error in errors)
+                elif isinstance(errors, str):
+                    message = errors
+                else:
+                    message = ''
+                raise UserError(gettext('certificate_manager.msg_error_loading_pkcs12',
+                        message=message))
+        return 'end'
